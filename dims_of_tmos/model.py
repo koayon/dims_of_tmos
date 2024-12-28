9657c9c740276f36e9a1c448822daea60897cb96
Kola Ayonrinde <koayon@gmail.com>
2024-12-28 23:22:13 +1100

    Allow saving and loading of models

--- dims_of_tmos/model.py
+++ dims_of_tmos/model.py
@@ -1,4 +1,7 @@
-from dataclasses import dataclass
+import json
+from dataclasses import asdict, dataclass
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
 import torch as t
@@ -17,6 +20,9 @@ class Config:
 
     # We could potentially use t.vmap instead.
     n_instances: int
+
+    def to_dict(self):
+        return asdict(self)
 
 
 class Model(nn.Module):
@@ -44,6 +50,10 @@ class Model(nn.Module):
             importance = t.ones(())
         self.importance = importance.to(device)
 
+    @classmethod
+    def base_path(cls) -> Path:
+        return Path("models")
+
     def forward(self, features: t.Tensor) -> t.Tensor:
         # features: [..., instance, n_features]
         # W: [instance, n_features, n_hidden]
@@ -67,3 +77,36 @@ class Model(nn.Module):
             t.zeros((), device=self.W.device),
         )
         return batch
+
+    def save(self, path: Optional[str]):
+        # Define path
+        base_path = self.base_path()
+        if path is None:
+            current_date = datetime.now().strftime("%Y-%m-%d_%H-%M")
+            _path = base_path / current_date
+        else:
+            _path = base_path / path
+
+        # Save config
+
+        config_path = _path / "config.json"
+        config_path.parent.mkdir(parents=True, exist_ok=True)
+        json.dump(self.config.to_dict(), config_path.open("w"))
+
+        # Save model
+        model_path = _path / "model.pt"
+        self.eval()
+        t.save(self.state_dict(), model_path)
+
+    @classmethod
+    def from_pretrained(cls, path: str) -> "Model":
+        config_path = cls.base_path() / path / "config.json"
+        config = Config(**json.load(config_path.open()))
+
+        model = cls(config)
+
+        model_path = cls.base_path() / path / "model.pt"
+        model.load_state_dict(t.load(model_path))
+        model.eval()
+
+        return model
