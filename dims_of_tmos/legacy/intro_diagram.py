1c343a9db77059b0cc0040c607b024e60b3bdb34
Kola Ayonrinde <koayon@gmail.com>
2024-12-29 00:33:32 +1100

    Move imports to the top of the file

--- dims_of_tmos/legacy/intro_diagram.py
+++ dims_of_tmos/legacy/intro_diagram.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch as t
+from matplotlib import collections as mc
+from matplotlib import colors as mcolors
 
 from dims_of_tmos import DEVICE
 from dims_of_tmos.model import Config, Model
@@ -10,23 +12,26 @@ from dims_of_tmos.train import optimize
 
 
 def plot_intro_diagram(model: Model):
-    from matplotlib import collections as mc
-    from matplotlib import colors as mcolors
 
     config = model.config
     WA = model.W.detach()
     # N = len(WA[:, 0])
     sel = range(config.n_instances)  # can be used to highlight specific sparsity levels
+
     plt.rcParams["axes.prop_cycle"] = plt.cycler(  # type: ignore
         "color", plt.cm.viridis(model.importance[0].cpu().numpy())  # type: ignore
     )
     plt.rcParams["figure.dpi"] = 200
     fig, axs = plt.subplots(1, len(sel), figsize=(2 * len(sel), 2))
+
     for i, ax in zip(sel, axs):
+        ax: plt.Axes  # type: ignore
+
         W = WA[i].cpu().detach().numpy()
         colors = [
             mcolors.to_rgba(c) for c in plt.rcParams["axes.prop_cycle"].by_key()["color"]
         ]
+
         ax.scatter(W[:, 0], W[:, 1], c=colors[0 : len(W[:, 0])])
         ax.set_aspect("equal")
         ax.add_collection(
@@ -38,10 +43,12 @@ def plot_intro_diagram(model: Model):
         ax.set_xlim((-z, z))
         ax.set_ylim((-z, z))
         ax.tick_params(left=True, right=False, labelleft=False, labelbottom=False, bottom=True)
+
         for spine in ["top", "right"]:
             ax.spines[spine].set_visible(False)
         for spine in ["bottom", "left"]:
             ax.spines[spine].set_position("center")
+
     plt.show()
 
 
