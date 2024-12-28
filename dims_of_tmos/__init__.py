115c9be4ece3ba48b8d56992a8ded8c360f87b64
Kola Ayonrinde <koayon@gmail.com>
2024-12-28 23:18:15 +1100

    Add base path

--- dims_of_tmos/__init__.py
+++ dims_of_tmos/__init__.py
@@ -1,6 +1,17 @@
+import os
+
 import torch as t
 
 if t.cuda.is_available():
     DEVICE = "cuda"
 else:
     DEVICE = "cpu"
+
+base_dir_path = "/mnt/ssd-1/mechinterp/koayon/dims_of_tmos/charts/"
+if os.path.exists(base_dir_path):
+    BASE_DIR = base_dir_path
+else:
+    BASE_DIR = os.path.join(os.getcwd(), "figures")
+    if not os.path.exists(base_dir_path):
+        os.makedirs(BASE_DIR, exist_ok=True)
+        os.makedirs(BASE_DIR, exist_ok=True)
