ff47f1a9c70899545621c9e7e501e36c008b1801
Kola Ayonrinde <koayon@gmail.com>
2025-01-07 21:53:07 +0000

    Add script to train tiny model for testing (orange)

--- dims_of_tmos/feature_geometry/feature_geom_train.py
+++ dims_of_tmos/feature_geometry/feature_geom_train.py
@@ -8,7 +8,14 @@ LEFT_FEATURE_PROB = 9 / 20
 RIGHT_FEATURE_PROB = 11 / 20
 
 
-def train_feature_geom_models() -> Model:
+def train_feature_geom_models(
+    num_features=400,
+    num_neurons=30,
+    num_instances=100,
+    train_steps=2_000,
+    batch_size=2**13,
+) -> Model:
+
     config = Config(
         num_features=num_features,
         num_neurons=num_neurons,
@@ -35,5 +42,14 @@ def train_feature_geom_models() -> Model:
 
 
 if __name__ == "__main__":
-    model = train_feature_geom_models()
-    model.save("feature_geom_model")
+    # model = train_feature_geom_models()
+    # model.save("feature_geom_model")
+
+    model = train_feature_geom_models(
+        num_features=10,
+        num_neurons=3,
+        num_instances=5,
+        train_steps=100,
+        batch_size=32,
+    )
+    model.save("feature_geom_model_test")
